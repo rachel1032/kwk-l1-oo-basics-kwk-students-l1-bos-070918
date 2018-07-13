04136452c8ccd7771efad78c36e04da8c644b871
rachel1032 <racheles32@gmail.com>
2018-07-13 14:57:09 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
 # Make your shoe class here!
+
+class Shoe
+  
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  
+  def initialize (brand)
+    @brand = brand
+   # @size = size
+   # @material = material
+   # @condition = condition
+    
+    end
+    
+def cobble
+  puts shoe.condition = "old"
+  puts shoe.condition ="new"
+  puts "Your shoe is as good as new!"
+    end
+  end
+  
+   new_shoes = Shoe.new("Adidas")
+    puts new_shoes.brand
+   # puts new_shoes.color
+#    puts new_shoes.size
+ #   puts new_shoes.material
+ #   puts new_shoes.condition
+  
